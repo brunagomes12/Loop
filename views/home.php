4dbff1b225006e73876e2f615cb9d44e8aa26df0
Bruna Gomes <bruggomes79@gmail.com>
2019-11-28 23:57:27 -0300

    Melhoria na home.

--- views/home.php
+++ views/home.php
@@ -1,17 +1,18 @@
 <h1 style="text-align:center">Bem vindo ao LOOP Couses&Training</h1>
-
-<form method="post" action="login.php">
-  <label>Usuário:</label>
-  <input type="text" name="usuario" placeholder="Nome do usuário" class="form-control">
-  <br>
-  <label>Senha:</label>
-  <input type="password" name="senha" placeholder="Digite sua senha" class="form-control">
-  <br>
-  <input type="submit" value="Entrar" class="btn btn-success">
-</form>
-<?php if(isset($_GET['erro'])){ ?>
-  <br>
-  <div class="alert alert-danger" role="alert">
-    Usuário e/ou senha inválidos!
-  </div>
+<?php if (!isset($_SESSION['login'])) { ?>
+  <form method="post" action="login.php">
+    <label>Usuário:</label>
+    <input type="text" name="usuario" placeholder="Nome do usuário" class="form-control">
+    <br>
+    <label>Senha:</label>
+    <input type="password" name="senha" placeholder="Digite sua senha" class="form-control">
+    <br>
+    <input type="submit" value="Entrar" class="btn btn-success">
+  </form>
+  <?php if(isset($_GET['erro'])){ ?>
+    <br>
+    <div class="alert alert-danger" role="alert">
+      Usuário e/ou senha inválidos!
+    </div>
+  <?php } ?>
 <?php } ?>
