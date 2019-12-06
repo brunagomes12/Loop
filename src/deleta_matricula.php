2e52b6174ebb6fb9a9f40b5f54de1543f8708267
Bruna Gomes <bruggomes79@gmail.com>
2019-12-05 21:20:15 -0300

    Inserindo código fonte na pasta src.

--- src/deleta_matricula.php
+++ src/deleta_matricula.php
@@ -2,7 +2,7 @@
 
 include 'database/db.php';
 
-$id_aluno_curso = $_GET['id_aluno_curso'];
+$id_aluno_curso = $_GET['id_matricula'];
 
 $query = "DELETE FROM aluno_curso WHERE id_aluno_curso = $id_aluno_curso";
 
